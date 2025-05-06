6e5ecb75ce3ab02908436fb2bc74f2af7b75e1f1
Khang Hy <153449985+CaptainHy1@users.noreply.github.com>
2025-05-07 00:08:37 +0700

    Delete target directory

--- target/QuanLyThucDon/assets/js/about.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.mobile-menu-toggle').addEventListener('click', function() {
-    const nav = document.querySelector('.horizontal-nav');
-    nav.classList.toggle('active');
-});
